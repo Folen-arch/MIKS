7495ba8430a3ee070b3b2a1c82bee909666daa22
DKuzmenko <marder1234567@gmail.com>
2023-11-29 12:16:13 +0200

    lab3 done

---
+++ lab3/problem1/order.js
@@ -0,0 +1,41 @@
+// order.js
+function placeOrder() {
+    const shawarmaType = document.getElementById("shawarma-type").value;
+    const selectedAddOns = Array.from(document.getElementById("add-ons").selectedOptions).map((option) => option.value);
+    const quantity = parseInt(document.getElementById("quantity").value, 10) || 1;
+
+    let shawarma;
+
+    if (shawarmaType === "turkish") {
+        shawarma = new TurkishShawarma();
+    } else if (shawarmaType === "dubai") {
+        shawarma = new DubaiShawarma();
+    }
+
+    selectedAddOns.forEach((addOn) => {
+        switch (addOn) {
+            case "hummus":
+                shawarma = new HummusDecorator(shawarma);
+                break;
+            case "garlic-sauce":
+                shawarma = new GarlicSauceDecorator(shawarma);
+                break;
+            // Add more cases for other add-ons if needed
+        }
+    });
+
+    const shawarmaCost = shawarma.calculateCost();
+    let totalCost = shawarmaCost * quantity;
+
+    if (totalCost > 200) {
+        totalCost += 2 * 0.99;
+    }
+
+    let orderSummary = `Ordered: ${quantity} ${shawarma.getDescription()} | Total Cost: ${totalCost.toFixed(2)} грн`;
+
+    if (totalCost > 200) {
+        orderSummary += " | 2 Americano Coffees";
+    }
+
+    document.getElementById("order-summary").innerText = orderSummary;
+}
